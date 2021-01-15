42c75332b773cb869b6fdd1555115070eaee3f88
Igor Vasylytsya <vasylytsya.igor@gmail.com>
2021-01-15 20:51:40 +0200

    litle change

--- src/components/AppStyled.js
+++ src/components/AppStyled.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 const Container = styled.div`
-  width: 500px;
-  margin-top: 25px;
-  border-radius: 20px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   border: 2px solid black;
   padding: 20px;
+  width: 400px;
+  border-radius: 20px;
 `;
 
 export default Container;
